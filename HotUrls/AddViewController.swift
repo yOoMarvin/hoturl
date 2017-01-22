49f6b6fa22161f159df737a910538afff7565dbf
Marvin Messenzehl <marvin.messenzehl@gmail.com>
2017-01-22 15:09:55 +0100

    Added audio recording feature in viewController

--- HotUrls/AddViewController.swift
+++ HotUrls/AddViewController.swift
@@ -7,27 +7,102 @@
 //
 
 import UIKit
+import Speech
 
 class AddViewController: UIViewController {
 
+    
+    //speech input handled in controller here... 
+    //should not be done in real world up but is ok for this purpose
+    
+    private let audioEngine = AVAudioEngine()
+    private let speechRecognizer = SFSpeechRecognizer(locale: Locale.current)!
+    
+    private var recognitionRequest: SFSpeechAudioBufferRecognitionRequest?
+    private var recognitionTask: SFSpeechRecognitionTask?
+    
     //outlets for ui elements
     @IBOutlet weak var nameAudioBtn: UIButton!
     @IBOutlet weak var urlAudioBtn: UIButton!
+    
     @IBOutlet weak var nameInput: UITextField!
     @IBOutlet weak var urlInput: UITextField!
     
-    //button action
+    //button actions
+    @IBAction func nameButtonTapped(_ sender: Any) {
+        speechInput(forField: nameInput, andButton: nameAudioBtn)
+    }
+    
+    @IBAction func urlButtonTapped(_ sender: Any) {
+        speechInput(forField: urlInput, andButton: urlAudioBtn)
+    }
+    
     @IBAction func saveTapped(_ sender: Any) {
     }
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+    
+    private func speechInput(forField: UITextField, andButton: UIButton) {
+        //if audio is running, stop it
+        if audioEngine.isRunning{
+            audioEngine.stop()
+            recognitionRequest?.endAudio()
+        } else {
+            do {
+            try getTranscription{
+                (transcript) in
+                //write value of transcript in textField
+                forField.text = transcript
+            }
+            } catch let error as NSError {
+                //todo: give error with alert
+                print(error.localizedDescription)
+            }
+        }
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    
+    
+    private func getTranscription(withHandler: @escaping(_ transcript: String) -> ()) throws{
+        
+        //kill old task
+        if let recognitionTask = recognitionTask {
+            recognitionTask.cancel()
+            self.recognitionTask = nil
+        }
+        
+        //record audio
+        let audioSession = AVAudioSession.sharedInstance()
+        try audioSession.setCategory(AVAudioSessionCategoryRecord)
+        try audioSession.setMode(AVAudioSessionModeMeasurement)
+        try audioSession.setActive(true, with: .notifyOthersOnDeactivation)
+        
+        //request
+        recognitionRequest = SFSpeechAudioBufferRecognitionRequest()
+        
+        guard let inputNode = audioEngine.inputNode else {
+            //todo: alert
+            print("no input node found")
+            
+            return
+        }
+        
+        guard let recognitionRequest = recognitionRequest else {
+            print("Could not make request")
+            return
+        }
+        
+        recognitionRequest.shouldReportPartialResults = false
+        
+        //get mic action
+        let format = inputNode.outputFormat(forBus: 0)
+        inputNode.installTap(onBus: 0, bufferSize: 1024, format: format){
+            (buffer: AVAudioPCMBuffer, when: AVAudioTime) in
+                self.recognitionRequest?.append(buffer)
+        }
+        
+        //TODO: transform speech input to text
+        //task...
+        
     }
-}
+    
+   }
