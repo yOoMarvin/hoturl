d33932c1054955c52a9c273e8b896dd0f6d22dff
Marvin Messenzehl <marvin.messenzehl@gmail.com>
2017-01-23 19:43:21 +0100

    Added UrlViewController for detail view (web)

---
+++ HotUrls/UrlViewController.swift
@@ -0,0 +1,15 @@
+//
+//  UrlViewController.swift
+//  HotUrls
+//
+//  Created by Marvin Messenzehl on 23.01.17.
+//  Copyright © 2017 Marvin Messenzehl. All rights reserved.
+//
+
+import UIKit
+
+class UrlViewController: UIViewController {
+
+    @IBOutlet weak var webView: UIWebView!
+        
+}
